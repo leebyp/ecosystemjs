922c9ae4a9110bd9956582a76fda7a8b46e4e06f
Benjamin Lee <leebyp@gmail.com>
2014-05-20 02:26:39 -0700

    Plan showing correctly evenly spaced in html.

--- app.js
+++ app.js
@@ -14,8 +14,27 @@ app.controller('HomeController', function($scope, $http){
      "#***....##**....c........**#",
      "#*****.....###***.......*###",
      "############################"];
-  
-
+  // $scope.invalid = false;
+  // $scope.submit = function(){
+  //   for (var i=0; i<$scope.plan.length; i++){
+  //     if ($scope.plan[i].length !== $scope.width){
+  //       $scope.invalid = true;
+  //       return;
+  //     }
+  //     if (!$scope.plan[i].match(/^[#.c]/)){
+  //       $scope.invalid = true;
+  //       return
+  //     }
+  //   }
+  // }
+  $scope.arr = []
+  for (var i=0; i<$scope.plan.length; i++){
+    var row = [];
+    for (var j=0; j<$scope.plan[i].length; j++){
+      row.push($scope.plan[i][j]);
+    }
+    $scope.arr.push(row);
+  }
 })
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
